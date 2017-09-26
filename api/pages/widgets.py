bfb0a8e99ecfe49f9f37bb07418b6f3409efb92f
Daniel Gruno <humbedooh@apache.org>
2017-09-26 09:50:34 +0200

    for some reason, old UI tries widget 0

--- api/pages/widgets.py
+++ api/pages/widgets.py
@@ -30,7 +30,7 @@ def run(API, environ, indata, session):
     widgets = yaml.load(open("yaml/widgets.yaml"))
     
     page = indata['pageid']
-    if not page:
+    if not page or page == '0':
         page = widgets.get('defaultWidget', 'repos')
     if page in widgets['widgets']:
         yield json.dumps(widgets['widgets'][page])
