102f2cc050a0c7e1464ec72f0c3907241ab9c2b0
Daniel Gruno <humbedooh@apache.org>
2017-10-02 20:48:13 +0200

    add PUT/DELETE/PATCH for views

--- api/pages/views.py
+++ api/pages/views.py
@@ -22,6 +22,7 @@ This is the views (filters) list handler for Kibble
 import json
 import re
 import time
+import hashlib
 
 def run(API, environ, indata, session):
     
@@ -29,48 +30,120 @@ def run(API, environ, indata, session):
     if not session.user:
         raise API.exception(403, "You must be logged in to use this API endpoint! %s")
     
-    # Fetch all views for default org
+    method = environ['REQUEST_METHOD']
     dOrg = session.user['defaultOrganisation'] or "apache"
-    res = session.DB.ES.search(
-            index=session.DB.dbname,
-            doc_type="view",
-            size = 5000,
-            body = {
-                'query': {
-                    'term': {
-                        'email': session.user['email']
+    
+    # Are we adding a view?
+    if method == 'PUT':
+        viewID = hashlib.sha224( ("%s-%s-%s" % (time.time(), session.user['email'], dOrg) ).encode('utf-8') ).hexdigest()
+        sources = indata.get('sources', [])
+        name = indata.get('name', "unknown view")
+        public = indata.get('public', False)
+        if public:
+            if not (session.user['userlevel'] == 'admin' or dOrg in session.user['ownerships']):
+                raise API.exception(403, "Only owners of an organisation may create public views.")
+        doc = {
+            'id': viewID,
+            'email': session.user['email'],
+            'organisation': dOrg,
+            'sourceList': sources,
+            'name': name,
+            'created': int(time.time()),
+            'publicView': public
+        }
+        session.DB.ES.index(index=session.DB.dbname, doc_type="view", id = viewID, body = doc)
+        yield json.dumps({'okay': True, 'message': "View created"})
+    
+    # Are we editing (patching) a view?
+    if method == 'PATCH':
+        viewID = indata.get('id')
+        if viewID and session.DB.ES.exists(index=session.DB.dbname, doc_type="view", id = viewID):
+            doc = session.DB.ES.get(index=session.DB.dbname, doc_type="view", id = viewID)
+            if session.user['userlevel'] == 'admin' or doc['_source']['email'] == session.user['email']:
+                sources = indata.get('sources', [])
+                doc['_source']['sourceList'] = sources
+                session.DB.ES.index(index=session.DB.dbname, doc_type="view", id = viewID, body = doc['_source'])
+                yield json.dumps({'okay': True, 'message': "View updated"})
+            else:
+                raise API.exception(403, "You don't own this view, and cannot delete it.")
+        else:
+            raise API.exception(404, "We couldn't find a view with this ID.")
+    
+    # Removing a view?
+    if method == 'DELETE':
+        viewID = indata.get('id')
+        if viewID and session.DB.ES.exists(index=session.DB.dbname, doc_type="view", id = viewID):
+            doc = session.DB.ES.get(index=session.DB.dbname, doc_type="view", id = viewID)
+            if session.user['userlevel'] == 'admin' or doc['_source']['email'] == session.user['email']:
+                session.DB.ES.delete(index=session.DB.dbname, doc_type="view", id = viewID)
+                yield json.dumps({'okay': True, 'message': "View deleted"})
+            else:
+                raise API.exception(403, "You don't own this view, and cannot delete it.")
+        else:
+            raise API.exception(404, "We couldn't find a view with this ID.")
+            
+    
+    if method in ['GET', 'POST']:
+        # Fetch all views for default org
+        
+        res = session.DB.ES.search(
+                index=session.DB.dbname,
+                doc_type="view",
+                size = 5000,
+                body = {
+                    'query': {
+                        'term': {
+                            'email': session.user['email']
+                        }
                     }
                 }
-            }
-        )
-    
-    
-    # Are we looking at someone elses view?
-    if indata.get('view'):
-        viewID = indata.get('view')
-        if session.DB.ES.exists(index=session.DB.dbname, doc_type="view", id = viewID):
-            blob = session.DB.ES.get(index=session.DB.dbname, doc_type="view", id = viewID)
-            if blob['_source']['email'] != session.user['email']:
-                blob['_source']['name'] += " (shared by " +  blob['_source']['email'] + ")"
-                res['hits']['hits'].append(blob)
-    sources = []
-    for hit in res['hits']['hits']:
-        doc = hit['_source']
-        if doc['organisation'] != dOrg:
-            continue
-        if indata.get('quick'):
-            xdoc = {
-                'id': doc['id'],
-                'name': doc['name'],
-                'organisation': doc['organisation']
+            )
+        
+        
+        # Are we looking at someone elses view?
+        if indata.get('view'):
+            viewID = indata.get('view')
+            if session.DB.ES.exists(index=session.DB.dbname, doc_type="view", id = viewID):
+                blob = session.DB.ES.get(index=session.DB.dbname, doc_type="view", id = viewID)
+                if blob['_source']['email'] != session.user['email']:
+                    blob['_source']['name'] += " (shared by " +  blob['_source']['email'] + ")"
+                    res['hits']['hits'].append(blob)
+        sources = []
+        for hit in res['hits']['hits']:
+            doc = hit['_source']
+            if doc['organisation'] != dOrg:
+                continue
+            if indata.get('quick'):
+                xdoc = {
+                    'id': doc['id'],
+                    'name': doc['name'],
+                    'organisation': doc['organisation']
+                    }
+                sources.append(xdoc)
+            else:
+                sources.append(doc)
+        
+        allsources = []
+        if indata.get('sources', False):
+            res = session.DB.ES.search(
+                index=session.DB.dbname,
+                doc_type="source",
+                size = 5000,
+                body = {
+                    'query': {
+                        'term': {
+                            'organisation': dOrg
+                        }
+                    }
                 }
-            sources.append(xdoc)
-        else:
-            sources.append(doc)
-    
-    JSON_OUT = {
-        'views': sources,
-        'okay': True,
-        'organisation': dOrg
-    }
-    yield json.dumps(JSON_OUT)
+            )
+            for doc in res['hits']['hits']:
+                allsources.append(doc['_source'])
+        
+        JSON_OUT = {
+            'views': sources,
+            'sources': allsources,
+            'okay': True,
+            'organisation': dOrg
+        }
+        yield json.dumps(JSON_OUT)
