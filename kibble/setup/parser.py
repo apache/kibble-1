d99601665573aa7eca89c87fa7f4359babe704eb
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-24 16:11:43 -0700

    removed extra comments

--- kibble/setup/parser.py
+++ kibble/setup/parser.py
@@ -3,13 +3,10 @@ from configparser import ConfigParser
 class KibbleConfigParser(ConfigParser):
 
     def __init__(self, ini_file="kibble.ini"):
-        # Let me know how you'd like me to handle incorrectly
-        # configured ini file
         super().__init__()
         self.read(ini_file)
 
         # merge elasticsearch url
-        # Let me know if this is the format you were looking for 
         dbname = self["elasticsearch"]['dbname']
         host = self["elasticsearch"]['host']
         port = self["elasticsearch"]['port']
